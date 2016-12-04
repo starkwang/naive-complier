89f4c606dba32c42f4879abb1a295e2b3754f981
starkwang <381152119@qq.com>
2016-12-04 20:47:33 +0800

    fix

--- transformer.js
+++ transformer.js
@@ -4,24 +4,32 @@ function transformer(root) {
 
 function transformExpr(Expr) {
     if (Expr.operator) {
+        // Expr -> Expr + Term
+        //      |  Expr - Term
         return `(${Expr.operator} ${transformTerm(Expr.child[0])} ${transformExpr(Expr.child[1])})`;
     } else {
+        // Expr -> Term
         return transformTerm(Expr.child[0]);
     }
 }
 
 function transformTerm(Term) {
     if (Term.operator) {
+        // Term -> Term * Factor
+        //      |  Term / Factor
         return `(${Term.operator} ${transformFactor(Term.child[0])} ${transformTerm(Term.child[1])})`;
     } else {
+        // Term -> Factor
         return transformFactor(Term.child[0]);
     }
 }
 
 function transformFactor(Factor) {
     if (Factor.child[0].type == 'Expr') {
+        // Factor -> (Expr)
         return transformExpr(Factor.child[0])
     } else {
+        // Factor -> num
         return Factor.child[0];
     }
 }
